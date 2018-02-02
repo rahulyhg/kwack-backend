d5ab55805c93a80d3dd3445075a3050dbbccd0c7
sailirawnang <saili.rawnang@wohlig.com>
2018-02-02 19:03:10 +0530

    Follow follower flow is done

--- api/controllers/UserFollowController.js
+++ api/controllers/UserFollowController.js
@@ -1,11 +1,37 @@
 module.exports = _.cloneDeep(require("sails-wohlig-controller"));
 var controller = {
-        /**
+
+    getOneUserDetail: function (req, res) {
+        if (req.body) {
+            UserFollow.getOneUserDetail(req.body.userId, req.body.GetUserId, req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: "Invalid Request"
+                }
+            })
+        }
+    },
+
+    getAllFollwersNameForInviteFrnd: function (req, res) {
+        if (req.body) {
+            UserFollow.getAllFollwersNameForInviteFrnd(req.body.userId, req.body.userFollower, req.body, res.callback);
+        } else {
+            res.json({
+                value: false,
+                data: {
+                    message: "Invalid Request"
+                }
+            })
+        }
+    },
+    /**
      * for get all User
      */
     getAllUser: function (req, res) {
         if (req.body) {
-            UserFollow.getAllUser(req.body.userId,req.body, res.callback);
+            UserFollow.getAllUser(req.body.userId, req.body, res.callback);
         } else {
             res.json({
                 value: false,
@@ -15,9 +41,9 @@ var controller = {
             })
         }
     },
-       areBothFollowing: function (req, res) {
+    areBothFollowing: function (req, res) {
         if (req.body) {
-            UserFollow.areBothFollowing(req.body.user,req.body.userBeenFollowed, res.callback);
+            UserFollow.areBothFollowing(req.body.user, req.body.userBeenFollowed, res.callback);
         } else {
             res.json({
                 value: false,
@@ -27,7 +53,7 @@ var controller = {
             })
         }
     },
-     getAllFollowerName: function (req, res) {
+    getAllFollowerName: function (req, res) {
         if (req.body) {
             UserFollow.getAllFollowerName(req.body.userId, res.callback);
         } else {
@@ -39,9 +65,9 @@ var controller = {
             })
         }
     },
-     getAllFollowingName: function (req, res) {
+    getAllFollowingName: function (req, res) {
         if (req.body) {
-            UserFollow.getAllFollowingName(req.body.userId, res.callback);
+            UserFollow.getAllFollowingName(req.body.userId,req.body, res.callback);
         } else {
             res.json({
                 value: false,
@@ -51,12 +77,12 @@ var controller = {
             })
         }
     },
-                 /**
+    /**
      *  to add Follow and Following count for User
      */
- addFollowerCount: function (req, res) {
+    addFollowerCount: function (req, res) {
         if (req.body) {
-            UserFollow.addFollowerCount(req.body.userFollowed,req.body.userFollwing, res.callback);
+            UserFollow.addFollowerCount(req.body.userFollowed, req.body.userFollwing, res.callback);
         } else {
             res.json({
                 value: false,
@@ -66,9 +92,9 @@ var controller = {
             })
         }
     },
-     removeFollowerCount: function (req, res) {
+    removeFollowerCount: function (req, res) {
         if (req.body) {
-            UserFollow.removeFollowerCount(req.body.userFollowed,req.body.userFollwing, res.callback);
+            UserFollow.removeFollowerCount(req.body.userFollowed, req.body.userFollwing, res.callback);
         } else {
             res.json({
                 value: false,
